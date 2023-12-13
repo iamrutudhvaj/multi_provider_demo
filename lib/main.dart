7c3f460f31bd75a7239521d402717ca8b3bf4c2b
Rutudhvaj Bodar <iamrutudhvaj@gmail.com>
2023-12-13 20:07:32 +0530

    step-1

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+String now() => DateTime.now().toIso8601String();
+
+class SecondsWidget extends StatelessWidget {
+  const SecondsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final seconds = context.watch<Seconds>();
+    return Expanded(
+        child: Container(
+      height: 100,
+      color: Colors.amber,
+      child: Text(seconds.value),
+    ));
+  }
+}
+
+class MinutesWidget extends StatelessWidget {
+  const MinutesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final minutes = context.watch<Minutes>();
+    return Expanded(
+        child: Container(
+      height: 100,
+      color: Colors.blue,
+      child: Text(minutes.value),
+    ));
+  }
+}
+
+@immutable
+class Seconds {
+  final String value;
+  Seconds() : value = now();
+}
+
+@immutable
+class Minutes {
+  final String value;
+  Minutes() : value = now();
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: MultiProvider(
+        providers: [
+          StreamProvider.value(
+            value: Stream<Seconds>.periodic(
+              const Duration(seconds: 1),
+              (_) => Seconds(),
+            ),
+            initialData: Seconds(),
+          ),
+          StreamProvider.value(
+            value: Stream<Minutes>.periodic(
+              const Duration(minutes: 1),
+              (_) => Minutes(),
+            ),
+            initialData: Minutes(),
+          ),
+        ],
+        child: const Row(
+          children: [
+            SecondsWidget(),
+            MinutesWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
